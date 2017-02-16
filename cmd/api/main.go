2de1ef92e6faa766b7f9c3a448e8ed293028ffcd
Jamu Kakar <jkakar@kakar.ca>
2017-02-15 20:32:09 -0800

    Add simple web server.

---
+++ cmd/api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/joeshaw/envdecode"
+)
+
+type config struct {
+	Port int `env:"PORT,default=5000"`
+
+	AWS struct {
+		Region      string `env:"AWS_REGION,required"`
+		AccessKeyID string `env:"AWS_ACCESS_KEY_ID,required"`
+		Bucket      string `env:"S3_BUCKET,default=thingsleilasays"`
+		ObjectName  string `env:"S3_OBJECT_NAME,default=tweets.json"`
+	}
+}
+
+func main() {
+	var cfg config
+	if err := envdecode.Decode(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil)
+}
