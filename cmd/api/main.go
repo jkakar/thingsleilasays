3ad7223aeb5c15f1cc629a03800d8c743deeeef0
Jamu Kakar <jkakar@kakar.ca>
2017-02-15 20:45:28 -0800

    Update favicon and add missing AWS_SECRET_ACCESS_KEY config.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,10 +12,11 @@ type config struct {
 	Port int `env:"PORT,default=5000"`
 
 	AWS struct {
-		Region      string `env:"AWS_REGION,required"`
-		AccessKeyID string `env:"AWS_ACCESS_KEY_ID,required"`
-		Bucket      string `env:"S3_BUCKET,default=thingsleilasays"`
-		ObjectName  string `env:"S3_OBJECT_NAME,default=tweets.json"`
+		Region          string `env:"AWS_REGION,required"`
+		AccessKeyID     string `env:"AWS_ACCESS_KEY_ID,required"`
+		SecretAccessKey string `env:"AWS_SECRET_ACCESS_KEY,required"`
+		Bucket          string `env:"S3_BUCKET,default=thingsleilasays"`
+		ObjectName      string `env:"S3_OBJECT_NAME,default=tweets.json"`
 	}
 }
 
